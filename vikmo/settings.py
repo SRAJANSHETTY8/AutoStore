7b2fce87598251531d892d4e08191e71a5705da4
SRAJANSHETTY8 <shettysrajan760@gmail.com>
2026-06-23 14:46:57 +0530

    settings fix

--- vikmo/settings.py
+++ vikmo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vqnx$!zux4w@muse!7s5_dg*hxj5x5wjgq*c3z=9ph-pgsdg7t'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
